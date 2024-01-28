e4479110f9f93b59b305320719624e3b121c59e0
chitra <chitrakarwate21@navgurukul.org>
2024-01-28 12:35:13 +0530

    ui is complete

---
+++ app/container.js
@@ -0,0 +1,39 @@
+const Container=()=>{
+    return(
+        <>
+                <div className="container-fluid">
+                    <div className=" mx-auto row_1  justify-content-start align-item-center border-bottom-dark mt-4 border">
+                        <div className="col-2 col_1 border border-dark"></div>
+                        <div className="row mx-auto border row_2 border border-dark">
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                           
+                        </div>
+                        <div className="row mx-auto border border-dark row_2">
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                           
+                        </div>
+                        <div className="row mx-auto border border-dark row_2">
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                            <div className="col-3 card m-3 border border-dark box1"></div>
+                           
+                        </div>
+                        <div className="col p-5 d-flex justify-content-center">
+                            <div className="col-5 p-3">
+                                <img src="Frame 13.png"/>
+                            </div>
+                        </div>
+                        
+                         
+                    </div>
+                </div>
+            
+        </>
+        
+    )
+}
+export default Container;
